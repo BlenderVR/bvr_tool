204e79073812787e3eadd720df9da25e2bc84685
Laurent Pointal <laurent.pointal@limsi.fr>
2016-04-12 10:13:40 +0200

    Structured parts in ad-hoc modules.

---
+++ bvrenv.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+# file: bvr/operator.py
+
+## Copyright (C) LIMSI-CNRS (2016)
+##
+## contributor(s) : Jorge Gascon, Damien Touraine, David Poirier-Quinot,
+## Laurent Pointal, Julian Adenauer,
+##
+## This software is a computer program whose purpose is to distribute
+## blender to render on Virtual Reality device systems.
+##
+## This software is governed by the CeCILL  license under French law and
+## abiding by the rules of distribution of free software.  You can  use,
+## modify and/ or redistribute the software under the terms of the CeCILL
+## license as circulated by CEA, CNRS and INRIA at the following URL
+## "http://www.cecill.info".
+##
+## As a counterpart to the access to the source code and  rights to copy,
+## modify and redistribute granted by the license, users are provided only
+## with a limited warranty  and the software's author,  the holder of the
+## economic rights,  and the successive licensors  have only  limited
+## liability.
+##
+## In this respect, the user's attention is drawn to the risks associated
+## with loading,  using,  modifying and/or developing or reproducing the
+## software by the user in light of its specific status of free software,
+## that may mean  that it is complicated to manipulate,  and  that  also
+## therefore means  that it is reserved for developers  and  experienced
+## professionals having in-depth computer knowledge. Users are therefore
+## encouraged to load and test the software's suitability as regards their
+## requirements in conditions enabling the security of their systems and/or
+## data to be ensured and,  more generally, to use and operate it in the
+## same conditions as regards security.
+##
+## The fact that you are presently reading this means that you have had
+## knowledge of the CeCILL license and that you accept its terms.
+
+# <pep8 compliant>
+
+"""Manage BlenderVR environment.
+
+This module prepare the environment for using BlenderVR console,
+it setup globals to reference ad-hoc directories (preferences/config
+location, installation path for blendervr, etc), install global names 
+in Python builtins, and adjust python path to access blendervr package.
+"""
+
+# ===== Normal module imports.
+# Load needed standard modules.
+import os
+import sys
+from os import path as osp
+import functools
+import builtins     # Important: we modify builtins to add our stuff!
+ 
+
+DEBUG = True
+
+# Constants.
+CONFIG_FILENAME = "profile_1.0.pickle"
+
+
+# Globals (ajusted in setup_environment())
+user_config_dir = ""
+blendervr_config_dir = ""
+blendervr_config_file = ""
+
+
+def setup_environment():
+    """Prepare the environment to work with blendervr console.
+    """
+    global user_config_dir
+    global blendervr_config_dir
+    global blendervr_config_file
+
+    package_dir = osp.dirname(osp.abspath(__file__))
+
+    # --- Find root of config directory for the system.
+    if 'XDG_CONFIG_HOME' in os.environ:
+        user_config_dir = os.environ['XDG_CONFIG_HOME']
+    elif sys.platform.startswith("win"):
+        user_config_dir = osp.join(os.environ['APPDATA'])
+    elif sys.platform == 'darwin':
+        user_config_dir = osp.join(osp.expanduser('~'), 'Library',
+                                            'Application Support')
+    elif sys.platform == 'linux':
+        user_config_dir = osp.join(osp.expanduser('~'), ".config")
+    else:
+        user_config_dir = osp.join(osp.expanduser('~'), ".config")
+    if DEBUG:
+        print("user_config_dir", repr(user_config_dir))
+    # Use a subdirectory blender/vr.
+    blendervr_config_dir = osp.join(user_config_dir, "blender", "vr")
+    if DEBUG:
+        print("blendervr_config_dir", blendervr_config_dir)
+    if not osp.isdir(blendervr_config_dir):         # Ensure it exists.
+        os.makedirs(blendervr_config_dir)
+
+    # Currently we use a fixed configuration file name.
+    blendervr_config_file = osp.join(blendervr_config_dir, CONFIG_FILENAME)
+
+    # Ensure we have a modules/ subdirectory in preferences, make it available to blendervr.
+    # TODO: rename modules_dir to pofiles_dir ?
+    modules_dir = os.path.join(blendervr_config_dir, 'modules')
+    if not osp.isdir(modules_dir): 
+        os.makedirs(modules_dir)
+    # /!\ Inject BlenderVR_profilePath in builtins
+    builtins.BlenderVR_profilePath = modules_dir
+
+    # TODO: (or not) retrieve root path from command-line… from configuration file.
+    root_dir = osp.join(package_dir, "source")
+    blendervr_dir = osp.join(root_dir, "modules")
+    # /!\ Inject BlenderVR_root in builtins
+    builtins.BlenderVR_root = root_dir
+
+    # Allow to import and use blendervr package.
+    if blendervr_dir not in sys.path:
+        sys.path.append(blendervr_dir)
+
+
+# This is called immediatly to benefit from globals values in our
+# Blender objects definitions.
+setup_environment()
+
+
+
---
+++ bvrprops.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+# file: bvr/bvrprops.py
+
+## Copyright (C) LIMSI-CNRS (2016)
+##
+## contributor(s) : Jorge Gascon, Damien Touraine, David Poirier-Quinot,
+## Laurent Pointal, Julian Adenauer,
+##
+## This software is a computer program whose purpose is to distribute
+## blender to render on Virtual Reality device systems.
+##
+## This software is governed by the CeCILL  license under French law and
+## abiding by the rules of distribution of free software.  You can  use,
+## modify and/ or redistribute the software under the terms of the CeCILL
+## license as circulated by CEA, CNRS and INRIA at the following URL
+## "http://www.cecill.info".
+##
+## As a counterpart to the access to the source code and  rights to copy,
+## modify and redistribute granted by the license, users are provided only
+## with a limited warranty  and the software's author,  the holder of the
+## economic rights,  and the successive licensors  have only  limited
+## liability.
+##
+## In this respect, the user's attention is drawn to the risks associated
+## with loading,  using,  modifying and/or developing or reproducing the
+## software by the user in light of its specific status of free software,
+## that may mean  that it is complicated to manipulate,  and  that  also
+## therefore means  that it is reserved for developers  and  experienced
+## professionals having in-depth computer knowledge. Users are therefore
+## encouraged to load and test the software's suitability as regards their
+## requirements in conditions enabling the security of their systems and/or
+## data to be ensured and,  more generally, to use and operate it in the
+## same conditions as regards security.
+##
+## The fact that you are presently reading this means that you have had
+## knowledge of the CeCILL license and that you accept its terms.
+
+# <pep8 compliant>
+
+"""Storage of BlenderVR variables within Blender properties.
+"""
+
+# ===== Normal module imports.
+# Load needed standard modules.
+import os
+import sys
+from os import path as osp
+import functools
+import builtins     # Important: we modify builtins to add our stuff!
+
+# Load necessary stuff from blender.
+import bpy
+
+#from bpy.props import (
+#        StringProperty,
+#        EnumProperty,
+#        PointerProperty,
+#        BoolProperty
+#        )
+#from bpy.types import (
+#        PropertyGroup
+
+# Load our environment settings (include standard config access path).
+from . import (
+        bvrenv,
+        )
+
+
+DEBUG = True
+
+# TODO: If necessary, setup a generic callback for properties update, allowing to
+# install handlers at late time (unless Blender has API for that), to manage
+# GUI control updates.
+# def propupdt_callback(propname, self, context):
+# And use with:
+# xxx = bpy.props.YYYProperty(…, update=functools.partial(propupdt_callback, propname=xxx))
+
+class BlenderVRProps(bpy.types.PropertyGroup):
+    """properties for a BlenderVR blender's extension.
+    
+    We use:
+        config_file_path
+        screen_setup
+        blend_scene_file_path
+        processor_file_path
+    """
+    
+    # Following properties are saved and loaded from configuration file
+    config_file_path = bpy.props.StringProperty(
+            name="BlenderVR Configuration File Path",
+            description="Path to the .xml configuration file",
+            default=osp.join(bvrenv.blendervr_config_dir, "main_pers1.1.xml"),
+            maxlen=1024, subtype="FILE_PATH",
+            )
+    screen_setup = bpy.props.EnumProperty(
+            name="Screen",
+            description="VR architecture screen setup",
+            items=(('console_debug', "Console Debug", ""),
+                   ('dk2_debug', "DK2 Debug", "")),
+            default='console_debug',
+            )
+
+
+    blend_scene_file_path = bpy.props.StringProperty(
+            name="Blender File Path",
+            description="Path to the blend scene",
+            default="//", 
+            maxlen=1024, subtype="FILE_PATH",
+            )
+    processor_file_path = bpy.props.StringProperty(
+            name="Processor File Path",
+            description="Path to the processor file",
+            default="//", 
+            maxlen=1024, subtype="FILE_PATH",
+            )
+
+    # Following properties are used for GUI interaction.
+    
+    
+    # Following properties are used at BlenderVR running time for feedback.
+    
+
+
+def register():
+    """Register classes of this submodule."""
+    bpy.utils.register_class(BlenderVRProps)
+
+
+def unregister():
+    """Unregister classes of this submodule."""
+    bpy.utils.unregister_class(BlenderVRProps)
+
